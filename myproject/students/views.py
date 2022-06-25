4c91787d6d7d0f55b195f4fa9d547a57fbf6ffb4
Your Name <chrisotelios@gmail.com>
2022-06-25 13:26:56 +0300

    requirements

--- myproject/students/views.py
+++ myproject/students/views.py
@@ -1,23 +1,7 @@
-from dataclasses import fields
-import imp
-from django.contrib.auth import login
-from django.shortcuts import redirect, render
-from django.urls import reverse
-from students.forms import StudentSignUpForm
-from django.http import HttpResponse  
-from django.shortcuts import render, redirect  
-from django.contrib.auth import login, authenticate  
-from django.contrib.sites.shortcuts import get_current_site  
-from django.utils.encoding import force_bytes, force_text  
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
-from django.template.loader import render_to_string  
-from django.core.mail import EmailMessage 
-from .token import account_activation_token  
-from django.contrib.auth import get_user_model
+from django.shortcuts import render
+from django.shortcuts import render 
 from students.models import Student
-# from courses.models import Course
 from applies.models import Apply
-from secretary.models import Secretary
 
 def apply(request):
     current_user = request.user
