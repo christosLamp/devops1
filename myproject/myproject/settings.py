5ade04efe02096198990e0bb8812bf583ad21bb1
Your Name <chrisotelios@gmail.com>
2022-06-29 22:22:45 +0300

    insert

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,3 +142,4 @@ AUTH_USER_MODEL = 'users.Myuser'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = BASE_DIR / "static"
