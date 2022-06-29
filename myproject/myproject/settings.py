234da8e6694d60acc8a136eda6ba8fecb25928c1
Your Name <chrisotelios@gmail.com>
2022-06-29 22:41:27 +0300

    insert

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -140,6 +140,8 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
 AUTH_USER_MODEL = 'users.Myuser'
 
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
